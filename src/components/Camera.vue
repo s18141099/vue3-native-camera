8f4fa607d133552707032c2becd9df6e2bff31a6
Chikawa <s18141099@gmail.com>
2023-11-24 16:31:38 +0900

    first commit

---
+++ src/components/Camera.vue
@@ -0,0 +1,44 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const camera = ref<HTMLInputElement | null>()
+
+const { timeout } = withDefaults(defineProps<{
+    timeout: number
+}>(), {
+    timeout: 300
+})
+
+const emits = defineEmits<{
+    (e: "onSnapshot", v: string): void
+}>()
+
+defineExpose({
+    onOpen,
+})
+
+function onOpen(): void {
+    setTimeout(() => {
+        camera.value?.click()
+    }, timeout)
+}
+
+function onChange(e: Event): void {
+    const inputElement = e.target as HTMLInputElement
+    const files = inputElement.files
+    if (!files || files.length === 0) return
+
+    const fReader = new FileReader()
+    fReader.readAsDataURL(files[0])
+    fReader.onload = (event) => {
+        const result = (event.target as FileReader).result
+        emits("onSnapshot", String(result))
+    }
+}
+</script>
+
+<template>
+    <input type="file" ref="camera" capture="environment" accept="image/*" hidden @change="onChange">
+</template>
+
+<style scoped></style>
