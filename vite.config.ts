cd96146432ca04193cdc415638bfbbf572bc27bf
Chikawa <s18141099@gmail.com>
2023-11-29 16:10:00 +0900

    #

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
   build: {
     lib: {
       entry: resolve(__dirname, './src/index.ts'),
-      name: 'vue3-camera',
+      name: 'vue3-native-camera',
       fileName: "index"
     },
     rollupOptions: {
